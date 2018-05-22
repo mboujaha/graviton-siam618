ed0605211a0fbe9a2eca140b01d3bde5a3569131
m.boujaha <mboujaha@gmail.com>
2018-05-22 11:03:08 +0000

    project restructuring layout components creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/shared/layout/sidebar/sidebar.component';
+import { NavbarComponent } from './components/shared/layout/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
